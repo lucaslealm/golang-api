a145a5f62c080bb16ca5b623708a2d033777c093
Lucas Martins <lucaslealm@gmail.com>
2020-06-09 17:19:38 -0300

    implemented test for GetDoctors

---
+++ test/get_doctors_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	routes "crud-api/routes"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetDoctors(t *testing.T) {
+	router := routes.SetupRouter()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/v1/doctors", nil)
+	router.ServeHTTP(w, req)
+	assert.Equal(t, 200, w.Code)
+}
